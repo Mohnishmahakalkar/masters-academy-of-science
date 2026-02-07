e78f6bd86860243fb3341c508391cdafeaa2e3f4
mmahakalkar <mmahakalkar@lumit.io>
2026-02-07 14:18:55 +0530

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ContentProvider } from "./data/contentStore";
 import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <ContentProvider>
         <App />
       </ContentProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
